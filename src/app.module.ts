ae9429324c0d3d145eb53b1baa1e8fe76c1ae016
mefissto <mefissto.w@gmail.com>
2019-12-19 23:32:17 +0200

    set up mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppConfig } from './utils/app.config';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(AppConfig.connectionString)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/utils/app.config.ts
@@ -0,0 +1,4 @@
+export class AppConfig {
+  public static connectionString =
+    'mongodb+srv://Mefissto:mefissto7526@nest-lrhy3.mongodb.net/test?retryWrites=true&w=majority';
+}
