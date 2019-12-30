c6b96741184693f337e0a9d6d0163702ad09b368
mefissto <mefissto.w@gmail.com>
2019-12-30 13:58:09 +0200

    added main components

--- client/src/app/system/system.module.ts
+++ client/src/app/system/system.module.ts
@@ -3,9 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { SystemComponent } from './system.component';
 import { SystemRoutingModule } from './system-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [SystemComponent],
-  imports: [CommonModule, SystemRoutingModule]
+  declarations: [
+    SystemComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContentComponent,
+    SearchComponent,
+    NavComponent,
+  ],
+  imports: [CommonModule, SystemRoutingModule],
 })
 export class SystemModule {}
